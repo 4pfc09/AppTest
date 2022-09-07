6532f8fa293c2f2d3dcd281f89b858a66598b745
4pfc09 <4pfc09@gmail.com>
2022-09-07 12:59:45 +0200

    Añadí una nueva activity con TabbedActivity con el esqueleto de Android.

--- app/src/main/java/com/example/myappemptyactivity/OptionsActivity.java
+++ app/src/main/java/com/example/myappemptyactivity/OptionsActivity.java
@@ -9,21 +9,39 @@ import android.widget.Button;
 
 public class OptionsActivity extends AppCompatActivity {
 
-    private Button btn;
+    private Button btnToCamera;
+    private Button btnToTabbedActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
 
-        btn = findViewById(R.id.buttonTest);
-        btn.setOnClickListener(new View.OnClickListener() {
+        btnToCamera = findViewById(R.id.btnToCamera);
+        btnToTabbedActivity = findViewById(R.id.btnToTabbedActivity);
+
+        btnToCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(view.getContext(), CameraActivity.class);
                 view.getContext().startActivity(i);
             }
         });
+        btnToCamera.setOnClickListener(v -> toCamera());
 
+
+
+        btnToTabbedActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), TabbedActivity.class);
+                view.getContext().startActivity(i);
+            }
+        });
+
+    }
+    private void toCamera() {
+        Intent i = new Intent(this, CameraActivity.class);
+        startActivity(i);
     }
 }
